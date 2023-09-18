a8aed046d51348268eb91091eea2bf5bb644b639
lilibella <lilibella28@hotmail.com>
2023-09-18 14:37:45 -0400

    setting song class with methods to display and create a song

--- src/audio-list/Song.cpp
+++ src/audio-list/Song.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
+#include <string>
+
+/*
+
+Create the Song class with attributes like title, artist, duration, and a pointer to the next song.
+Implement basic methods for creating and displaying songs.
+
+// keep data private for Encapsulation
+//keep member public
+
+*/
+
+
+class Song
+{
+    private: //keep data private(encapsulation)
+    std::string title;
+    std::string artist;
+    double duration;
+    
+    public:
+    void createSong(std::string songTitle, std::string artistName);
+    void displayingSong(std::string songTitle, std::string artistName);
+
+};
+
+
+void Song::createSong(std::string songTitle, std::string artistName){
+
+}
+
+void Song::displayingSong(std::string songTitle, std::string artistName){
+
+}
+
 
 
